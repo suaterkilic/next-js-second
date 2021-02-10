a63bbc07bb9fa792531e88b9b275ee8f689b5de7
suat erkilic <suat.php@gmail.com>
2021-02-10 19:21:40 +0300

    *fist-commit

---
+++ pages/index.js
@@ -0,0 +1,16 @@
+import Head from 'next/head'
+import styles from '../styles/Home.module.css'
+
+export default function Home() {
+  return (
+    <div>
+      <Head>
+        <title>Suat Page 2</title>
+        <link rel="stylesheet" href="/_next/static/css/suat.css" />
+      </Head>
+      <div className="suat-class">
+          SUAT ERKILIÇ
+      </div>
+    </div>
+  )
+}
